3095e47c351968fd641d9e7233a2ac6d15520c7d
ErenYeagerrrrrr <muhammadjonsobirov156@gmail.com>
2024-07-13 20:13:55 +0500

    card

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App ">
+      <div className='w-64 h-64 text-center mx-auto'>
+        <div className="bg-gray-300 w-16 h-16 rounded-full mx-auto my-10 "></div>
+        <h2 className="text-3xl font-bold">Headline</h2>
+        <p className="text-1xl mt-4      ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vel corrupti accusantium quam.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
